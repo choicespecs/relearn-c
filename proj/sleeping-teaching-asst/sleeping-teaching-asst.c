474a8a2d0539c0ccfb1711bcb7b2034859527b2e
christophermlee2 <christophermlee2@gmail.com>
2022-08-21 22:57:38 -0400

    added new project

---
+++ proj/sleeping-teaching-asst/sleeping-teaching-asst.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<stdbool.h>
+#include<pthread.h>
+
+bool isInteger(const char* string) {
+
+    while(*string != '\0') {
+        if (!isdigit(*string)) {
+            return false;
+        }
+        string++;
+    }
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    int students = 0;
+    if (argc != 2) {
+        printf("only define # of students as argument\n");
+        exit(EXIT_FAILURE);
+    }
+    if (!isInteger(argv[1])) {
+        printf("not a valid value for students\n");
+        exit(EXIT_FAILURE);
+    }
+    students = atoi(argv[1]);
+    printf("Created %i students\n", students);
+}
